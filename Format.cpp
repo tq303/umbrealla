b8322d6c5956eb6052cc97b46e8a743da7606ebf
unknown <contactoliverwhite@gmail.com>
2015-12-09 18:22:04 +0000

    format class definition

--- Format.cpp
+++ Format.cpp
@@ -1,28 +1,21 @@
-// options
-// 1 = umbrella
-// 1,1 = animation
-// 1,1,1 = colour
-// 1,1,1,1 = speed
-//
-
-void Format::umbrella(number) {
+void Format::umbrella(int number) {
     return getDigit(number, 3);
 }
 
-void Format::colour(number) {
+void Format::colour(int number) {
     return getDigit(number, 2);
 }
 
-void Format::animation(number) {
+void Format::animation(int number) {
     return getDigit(number, 1);
 }
 
-void Format::speed(number) {
+void Format::speed(int number) {
     return getDigit(number, 0);
 }
 
 // 0-based index pos
-int Format::getDigit (const long number, int pos) 
+int Format::getDigit (const long number, int pos)
 {
     return (pos == 0) ? number % 10 : getDigit (number/10, --pos);
 }
