42bea4166f5afd6d8c924ac07312b7bec0f67f45
unknown <contactoliverwhite@gmail.com>
2016-01-05 23:55:02 +0000

    test examples

---
+++ receive/receive.ino
@@ -0,0 +1,52 @@
+/*
+ * Receiver
+ */
+ 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN   9
+#define CSN_PIN 10
+
+// NOTE: the "LL" at the end of the constant is "LongLong" type
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+RF24 radio(CE_PIN, CSN_PIN);
+int joystick[2];
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Nrf24L01 Receiver Starting");
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+}
+
+
+void loop()
+{
+  if ( radio.available() )
+  {
+    // Read the data payload until we've received everything
+    bool done = false;
+    while (!done)
+    {
+      // Fetch the data payload
+      radio.read( joystick, sizeof(joystick) );
+      done = true;
+      Serial.print("X = ");
+      Serial.print(joystick[0]);
+      Serial.print(" Y = ");      
+      Serial.println(joystick[1]);
+    }
+  }
+  else
+  {    
+      Serial.println("No radio available");
+  }
+
+}
+
---
+++ send/send.ino
@@ -0,0 +1,34 @@
+/*
+ * Transmitter
+ */
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN   9
+#define CSN_PIN 10
+#define JOYSTICK_X A0
+#define JOYSTICK_Y A1
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+RF24 radio(CE_PIN, CSN_PIN);
+int joystick[2];
+
+void setup()
+{
+  Serial.begin(115200);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+}
+
+
+void loop()
+{
+  joystick[0] = xFFFFFF;
+  joystick[1] = 1024;
+  
+  radio.write( joystick, sizeof(joystick) );
+
+}
