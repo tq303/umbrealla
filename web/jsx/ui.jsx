d8e2ee16552b2d70b70ce3c630179d939f4d4e47
Oliver White <oliver.white@MOO-00157.local>
2016-02-09 17:55:14 +0000

    addition of processing-js

--- web/jsx/ui.jsx
+++ web/jsx/ui.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Processing from 'processing-js';
 
 require('../styles/ui.scss');
 
 class UI extends React.Component {
+    constructor(props) {
+        super(props);
+        window.console.log(Processing);
+    }
     render() {
         return <h1>This is the interface.</h1>
     }
