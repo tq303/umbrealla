595b4c0fd723e97fafe775bfe5ac34276680de2c
Oliver White <oliver.white@MOO-00157.local>
2016-02-10 13:31:56 +0000

    add, remove, scan, delete and undo of cycle array

--- web/jsx/UI/Cycle.jsx
+++ web/jsx/UI/Cycle.jsx
@@ -10,7 +10,9 @@ class Cycle extends React.Component {
     constructor() {
         super();
         this.state = {
-            animateCycles: []
+            animateCycles: [this.animateArray()],
+            cyclePosition: 1,
+            undoCycle: this.defaultUndoCycle()
         };
     }
     animateArray() {
@@ -26,23 +28,109 @@ class Cycle extends React.Component {
         }
         return array;
     }
+    defaultUndoCycle() {
+        return {
+            position: null,
+            cycle: null
+        }
+    }
     pushCycle() {
-        console.log('adding cycle');
+        if (this.state.animateCycles.length === this.state.cyclePosition) {
+            this.setState((state) => {
+                animateCycles: state.animateCycles.push( this.getLastCycle() )
+            });
+        }
+        this.setState((state) => {
+            cyclePosition: state.cyclePosition++
+        });
     }
     popCycle() {
-        console.log('removing cycle');
+        if (this.state.cyclePosition > 1) {
+            this.setState((state) => {
+                cyclePosition: state.cyclePosition--
+            });
+        }
+    }
+    insertCycle() {
+        this.setState((state) => {
+            animateCycles: state.animateCycles.splice( (state.cyclePosition - 1), 0, this.getCurrentCycle() )
+        });
+    }
+    deleteCycle() {
+        if (this.state.animateCycles.length > 1) {
+
+            this.state.animateCycles.splice( this.state.animateCycles.indexOf( this.getCurrentCycleRef() ), 1 );
+            let newCycles = this.state.animateCycles;
+
+            this.setState((state) => {
+                return {
+                    undoCycle: {
+                        position: (state.cyclePosition - 1),
+                        cycle: this.getCurrentCycle()
+                    },
+                    animateCycles: newCycles
+                }
+            });
+
+            if (this.state.cyclePosition > this.state.animateCycles.length) {
+                this.setState((state) => {
+                    cyclePosition: state.cyclePosition--
+                });
+            }
+
+        }
+    }
+    undoDeleteCycle() {
+        if (this.state.undoCycle.position !== null && this.state.undoCycle.cycle !== null) {
+
+            this.state.animateCycles.splice( this.state.undoCycle.position, 0, this.state.undoCycle.cycle );
+            let newCycles = this.state.animateCycles;
+
+            this.setState((state) => {
+                return {
+                    animateCycles: newCycles,
+                    undoCycle: this.defaultUndoCycle()
+                }
+            });
+
+        }
+    }
+    getLastCycle() {
+        return this.state.animateCycles.slice()[0];
+    }
+    getCurrentCycle() {
+        return this.state.animateCycles.slice(( this.state.cyclePosition - 1 ), 1)[0];
+    }
+    getCurrentCycleRef() {
+        return this.state.animateCycles[( this.state.cyclePosition - 1 )];
     }
     render() {
         return (
             <div id="cycle">
+
                 <div className="btn-group">
-                    <button type="button" className="btn btn-default" onClick={ this.popCycle }>
-                        <i className="glyphicon glyphicon-chevron-left"></i>
+                    <button type="button" className="btn btn-default" onClick={ this.popCycle.bind(this) }>
+                        <i className="fa fa-step-backward"></i>
                     </button>
-                    <button type="button" className="btn btn-default" onClick={ this.pushCycle }>
-                        <i className="glyphicon glyphicon-chevron-right"></i>
+                    <button type="button" className="btn btn-default" onClick={ this.pushCycle.bind(this) }>
+                        <i className="fa fa-step-forward"></i>
                     </button>
                 </div>
+
+                <span className="badge">{ this.state.cyclePosition }</span> / <span className="badge">{ this.state.animateCycles.length }</span>
+
+                <div className="btn-group">
+                    <button type="button" className="btn btn-default" onClick={ this.insertCycle.bind(this) }>
+                        <i className="fa fa-plus"></i>
+                    </button>
+                    <button type="button" className="btn btn-default" disabled={ this.state.animateCycles.length === 1 } onClick={ this.deleteCycle.bind(this) }>
+                        <i className="fa fa-minus"></i>
+                    </button>
+                    <button type="button" className="btn btn-default" disabled={ !this.state.undoCycle.position } onClick={ this.undoDeleteCycle.bind(this) }>
+                        <i className="fa fa-undo"></i>
+                    </button>
+                </div>
+
             </div>
         )
     }
