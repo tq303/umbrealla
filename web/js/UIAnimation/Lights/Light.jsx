fd4b0c5e5b9c9eddbaea762e100a1cb3598aa7b4
tq303 <contactoliverwhite@gmail.com>
2016-02-21 17:13:25 +0000

    set led colour active/inactive correct toggle colours

--- web/js/UIAnimation/Lights/Light.jsx
+++ web/js/UIAnimation/Lights/Light.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
 class Light extends React.Component {
+
+    constructor() {
+        super();
+
+        this.iColour = '#777';
+
+        this.state = {
+            active: false,
+            activeClass: '',
+            activeStyle: {
+                color: this.iColour
+            },
+            colour: '#337ab7'
+        }
+    }
+
+    toggleActive() {
+        this.setState(( state )=> {
+            return {
+                active: !state.active,
+                activeClass: ( state.active ) ? '' : 'active',
+                activeStyle: {
+                    color: ( state.active ) ? this.iColour : state.colour
+                }
+            }
+        });
+    }
+
+    setColour( colour ) {
+        this.setState({
+            activeStyle: {
+                color: colour
+            },
+            colour: colour
+        })
+    }
+
     render() {
         return (
-            <a></a>
+            <a className={ this.state.activeClass } style={ this.state.activeStyle } onClick={ this.toggleActive.bind(this) }></a>
         )
     }
 }
