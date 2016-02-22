26f630f1bf7a346ea75c70d97ded643083fe6ded
tq303 <contactoliverwhite@gmail.com>
2016-02-22 21:03:09 +0000

    configure de/select all lights from ui

--- web/js/Animator/UIAnimation/Lights/Light.jsx
+++ web/js/Animator/UIAnimation/Lights/Light.jsx
@@ -15,7 +15,7 @@ class Light extends React.Component {
                 color: ( this.props.colour !== null) ? this.props.colour : this.iColour
             },
             colour: this.props.colour
-        }
+        };
     }
 
     toggleActive() {
@@ -25,8 +25,22 @@ class Light extends React.Component {
                 activeClass: ( state.active ) ? '' : 'active',
                 activeStyle: {
                     color: ( state.active ) ? this.iColour : state.colour
-                }
+                },
+                colour: this.props.colour
             }
+        });
+    }
+
+    componentWillReceiveProps( nextProps ) {
+        this.setState(( state )=> {
+            return {
+                active: ( nextProps.colour !== null ),
+                activeClass: ( nextProps.colour !== null ) ? 'active' : '',
+                activeStyle: {
+                    color: ( nextProps.colour !== null) ? nextProps.colour : this.iColour
+                },
+                colour: nextProps.colour
+            };
         });
     }
 
